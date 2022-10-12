dcdb7cda7efbf5ff6a841aa8ba81374635257bc0
Deric F <dericgf@gmail.com>
2022-10-11 22:11:25 -0600

    Initial commit - add boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-white h-screen">
+      <header>
+        <h1>Placeholder</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
