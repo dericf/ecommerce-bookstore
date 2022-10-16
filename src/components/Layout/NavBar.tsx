aec1074f214e4799047fd09de58f5dce0373a911
Deric F <dericgf@gmail.com>
2022-10-16 04:42:47 -0600

    Show cart item count on cart nav link

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,21 +1,27 @@
+import { useAppSelector } from "hooks/storeHooks";
+import { ROUTES } from "lib/routes";
 import React from "react";
 import NavItem from "./NavItem";
 
 const NAV_ITEMS = [
-  { label: "Home", path: "/" },
-  { label: "Products", path: "/products" },
-  { label: "Cart", path: "/cart" },
+  { label: "Home", path: ROUTES.HOME },
+  { label: "Products", path: ROUTES.PRODUCTS },
 ];
 
 type Props = {};
 
 const NavBar = (props: Props) => {
+  const { items } = useAppSelector((state) => state.cart);
   return (
-    <nav className="w-screen bg-gray-veryLight py-2">
+    <nav className="w-screen bg-lightGray py-2 sticky top-0 z-10">
       <ul className="flex decoration-0">
         {NAV_ITEMS.map((link) => (
           <NavItem key={link.path} to={link.path} label={link.label} />
         ))}
+        <NavItem
+          to={ROUTES.CART}
+          label={items.length > 0 ? `Cart (${items.length})` : "Cart"}
+        />
       </ul>
     </nav>
   );
