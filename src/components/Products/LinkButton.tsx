fb5ad2a3a8619680835fb3e4daa5fd58ab2667f3
Deric F <dericgf@gmail.com>
2022-10-16 04:43:04 -0600

    Add proper overrides for linkbutton with tw merge

--- src/components/Products/LinkButton.tsx
+++ src/components/Products/LinkButton.tsx
@@ -4,17 +4,22 @@ import React, {
   PropsWithChildren,
 } from "react";
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+import { Color } from "types/general";
 
 type Props = {
   to: string;
+  color?: Color;
 };
 
 const LinkButton: FunctionComponent<
   PropsWithChildren<HTMLAttributes<HTMLButtonElement> & Props>
-> = ({ children, to, className = "" }) => {
+> = ({ children, to, className = "", color = "primary" }) => {
   return (
     <Link to={to}>
-      <button className={`button-outlined ${className}`}>{children}</button>
+      <button className={twMerge("button-outlined", className, color)}>
+        {children}
+      </button>
     </Link>
   );
 };
