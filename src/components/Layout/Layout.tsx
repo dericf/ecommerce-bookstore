4507ec1b4b1d62cf6b63e7e9345b3ef32da0800f
Deric F <dericgf@gmail.com>
2022-10-16 04:20:32 -0600

    Add toast notification

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, PropsWithChildren } from "react";
+import { ToastContainer } from "react-toastify";
 import NavBar from "./NavBar";
+import "react-toastify/dist/ReactToastify.css";
 
 type LayoutProps = {};
 
@@ -10,9 +12,14 @@ const Layout: FunctionComponent<PropsWithChildren<LayoutProps>> = ({
   return (
     <div className="w-screen h-screen mx-auto bg-white">
       <NavBar />
-      <main className="main-content container max-w-6xl px-8 py-16 mx-auto">
+      <main className="main-content container max-w-6xl px-8 py-10 mx-auto">
         {children}
       </main>
+      <ToastContainer
+        autoClose={3000}
+        position="bottom-right"
+        hideProgressBar
+      />
     </div>
   );
 };
