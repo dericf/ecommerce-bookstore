7eb15cd0949df9a2001d599c0068fa5441d41e8d
Deric F <dericgf@gmail.com>
2022-10-15 23:58:22 -0600

    Add redux dev tools in non-prod mode

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,6 +6,7 @@ export const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
